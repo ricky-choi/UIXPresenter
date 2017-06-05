3be9db54daf11bbc9394f44dd35e21678eb58176
Jaeyoung Choi <hideyf@gmail.com>
2017-06-05 15:40:15 +0900

    add executable target

---
+++ Presenter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Presenter
+//
+//  Created by Jaeyoung Choi on 2017. 6. 5..
+//  Copyright © 2017년 Appcid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
