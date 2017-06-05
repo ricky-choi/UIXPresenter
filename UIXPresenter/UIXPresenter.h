1488c9c1f31ab5ba5572decb74aea351e2febbac
Jaeyoung Choi <hideyf@gmail.com>
2017-06-05 15:37:55 +0900

    initial commit

---
+++ UIXPresenter/UIXPresenter.h
@@ -0,0 +1,19 @@
+//
+//  UIXPresenter.h
+//  UIXPresenter
+//
+//  Created by Jaeyoung Choi on 2017. 6. 5..
+//  Copyright © 2017년 Appcid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIXPresenter.
+FOUNDATION_EXPORT double UIXPresenterVersionNumber;
+
+//! Project version string for UIXPresenter.
+FOUNDATION_EXPORT const unsigned char UIXPresenterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIXPresenter/PublicHeader.h>
+
+
